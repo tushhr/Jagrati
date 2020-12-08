8b99a8351b882acdbd5d1a800fe74aa4eea7d2ce
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2020-12-08 13:12:44 +0530

    Added event home page

--- events/urls.py
+++ events/urls.py
@@ -6,5 +6,6 @@ app_name='events'
 
 urlpatterns = [
     path('', views.index, name='events'),
+    path('new_event', views.new_event, name='new_event'),
     path('add', views.add, name='add-event'),
 ]
