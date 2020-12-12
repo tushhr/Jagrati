df53583473a09a898821acf4f8f4811205ccfe09
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2020-12-12 20:58:03 +0530

    Load Image from Database

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
