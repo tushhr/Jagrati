3ff87bd59b1f33fd296b08f79b1d16dc8b8601ac
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2020-12-12 20:57:26 +0530

    Added Image Field

--- events/views.py
+++ events/views.py
@@ -47,26 +47,20 @@ def add(request):
 		venue=request.POST['venue']
 		description=request.POST['description']
 		organiser1=request.POST['organiser1']
-		#if there is an co-host
-		if request.POST['organiser2'] is not None:
-			organiser2 = request.POST['organiser2']
 		winner1=request.POST['winner1']
-		winner2=request.POST['winner2']
-		winner3=request.POST['winner3']
+		image = request.FILES['image']
 
 		#check whether event with same name exist or not
 		if events.objects.filter(event_name = event_name).exists():
 			messages.error(request, "Event with same name already exist!")
 		else:
-			event = events(event_name = event_name, date = date, venue = venue, description = description, winner1 = winner1, winner3 = winner3, winner2 = winner2)
+			event = events(event_name = event_name, date = date, venue = venue, description = description, winner1 = winner1, image = image)
 			event.save()
 			event.organiser.add(Profile.objects.get(roll_no = organiser1))
-			if organiser2:
-				event.organiser.add(Profile.objects.get(roll_no = organiser2))
-
+		
 			event.save()
 			messages.error(request, "Done Successfully!")
-			return redirect('')
+			return redirect('/')
 
 
 	return render(request, 'events/add_event.html')
