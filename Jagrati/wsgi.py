32e4e260edb9b454a7000f06c37f91faebc67736
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2021-05-09 09:52:08 +0530

    Whitenoise error fixed

--- Jagrati/wsgi.py
+++ Jagrati/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
-from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jagrati.settings')
 
-application = DjangoWhiteNoise(application)
+application = get_wsgi_application()
